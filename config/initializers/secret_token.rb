f4872d00a20c3c8c1898e0b022afa632844dc5eb
davemerritt <davidmerritt6@gmail.com>
2013-11-28 14:17:44 -0500

    Restarted rainforest, trying out scaffold and bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = '365c3306009202afda159697174b900b8d8065a8f88a7116ffda6f3e337dd19cef3c6a218a19aa1b2698b3b53c9781c3988c02024437598491449d91d8059537'
